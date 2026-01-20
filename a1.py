07079833635f197ae937514a4c3b7fd30674ad22
DenaSYNuuman <dena.sarhangyehudanuuman@york.ac.uk>
2026-01-20 22:48:43 +0000

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,10 @@
+q1_earnings = 12
+q2_earnings = 15
+q3_earnings = 11
+q4_earnings = 17
+total_earnings = q1_earnings + q2_earnings + q3_earnings + q4_earnings
+average_earnings = total_earnings / 4
+print( average_earnings )
+
+import sys
+print(sys.executable)
